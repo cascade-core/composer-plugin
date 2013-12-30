eff56541ee9ab014d3a825bc78d985c054ff8d03
Josef Kufner <jk@frozen-doe.net>
2013-12-30 22:53:30 +0100

    Simple cascade-plugin installer

---
+++ src/Cascade/ComposerPlugin/Installer.php
@@ -0,0 +1,39 @@
+<?php
+namespace Cascade\ComposerPlugin;
+
+use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
+
+class Installer extends LibraryInstaller
+{
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getInstallPath(PackageInterface $package)
+	{
+		$type = $package->getType();
+		$extra = $package->getExtra();
+		$plugin = @ $extra['plugin'];
+
+		if ($plugin == '') {
+			throw new \Exception('Plugin name not specified! Set extra.plugin option in composer.json to valid plugin name.');
+		}
+
+		if (!preg_match('/^[a-z0-9]([a-z0-9_]*[a-z0-9])?$/', $plugin)) {
+			throw new \Exception('Plugin name "'.$plugin.'" is invalid! Set extra.plugin option in composer.json to valid plugin name -- a-z, 0-9 and underscore only.');
+		}
+
+		return 'plugin/'.$plugin;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function supports($packageType)
+	{
+		return $packageType == 'cascade-plugin';
+	}
+
+}
+
